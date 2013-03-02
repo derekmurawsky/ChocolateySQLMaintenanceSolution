7ad7d6157b9a2c213315dfea78d827d4f15451cd
derekmurawsky <dmurawsky@gmail.com>
2013-03-02 16:52:10 -0500

    Rewrote installer to be cleaner
    Fixed issue ID #1, error in running invoke-sqlcmd

--- SQLMaintenanceSolution/tools/chocolateyInstall.ps1
+++ SQLMaintenanceSolution/tools/chocolateyInstall.ps1
@@ -3,28 +3,41 @@ $url = 'http://ola.hallengren.com/scripts/MaintenanceSolution.sql'
 $FileName = 'MaintenanceSolution.sql'
 $url64 = $url
 
-try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
-  # other helpers - using any of these means you want to uncomment the error handling up top and at bottom.
-  # downloader that the main helpers use to download items
-  $scriptPath = $(Split-Path $MyInvocation.MyCommand.Path)
-  $nodePath = Join-Path $scriptPath $FileName
-  Get-ChocolateyWebFile "$packageName" "$nodePath" "$url" "$url64"
-  
-  # Test if SQL server is running
-  if ((Get-Service MSSQLSERVER).status -ne "Running") {
-    Write-ChocolateyFailure "$packageName" "SQL Server not started"
-  	throw
-  }
-  if ((Get-Service SQLSERVERAGENT).status -ne "Running"){
-    Write-ChocolateyFailure "$packageName" "SQL Server Agent not started"
-  	throw
-  }
- 
+try {
+	# Tests  
+	if ((Get-Service MSSQLSERVER).status -ne "Running") {
+    	Write-ChocolateyFailure "$packageName" "SQL Server not started"
+  		throw
+	}
+	if ((Get-Service SQLSERVERAGENT).status -ne "Running"){
+    	Write-ChocolateyFailure "$packageName" "SQL Server Agent not started"
+  		throw
+	}
+	if ( (Get-PSSnapin -Name sqlserverprovidersnapin100 -ErrorAction SilentlyContinue) -eq $null ){
+		try {
+			Add-PsSnapin sqlserverprovidersnapin100
+		} catch {
+			Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+			throw
+		}
+	}
+	if ( (Get-PSSnapin -Name sqlservercmdletsnapin100 -ErrorAction SilentlyContinue) -eq $null ){
+		try {
+			Add-PsSnapin sqlservercmdletsnapin100
+		} catch {
+			Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+			throw
+		}
+	}
+	# If tests passed, initialize path, download, and install. 
+	$scriptPath = $(Split-Path $MyInvocation.MyCommand.Path)
+	$nodePath = Join-Path $scriptPath $FileName
+	Get-ChocolateyWebFile "$packageName" "$nodePath" "$url" "$url64"
+
 	invoke-sqlcmd -InputFile "$nodePath"
 
-  # the following is all part of error handling
-  Write-ChocolateySuccess "$packageName"
+	Write-ChocolateySuccess "$packageName"
 } catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw 
+	Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+	throw 
 }
